1e4e53021006cab9f522900b3b97d15703b264e2
Acidpersik <aromatik576@gmail.com>
2020-10-31 00:00:38 +0300

    add profile

--- blog_app/accounts/urls.py
+++ blog_app/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, CustomUserDetailView
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+path('accounts/<int:pk>/profile', CustomUserDetailView.as_view(), name='user-profile'),
 ]
--- blog_app/accounts/views.py
+++ blog_app/accounts/views.py
@@ -1,15 +1,17 @@
-# from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from .forms import AuthorCreationForm
+from .models import CustomUser
 
 
 class SignUpView(CreateView):
     form_class = AuthorCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
 
 
-
+class CustomUserDetailView(DetailView):
+    model = CustomUser
+    template_name = 'profile.html'
 
